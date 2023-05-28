52a5458e28f1687eb117c3aa18ce970b01c35bf7
joker <ser.fyodoroff@yandex.ru>
2023-05-29 00:06:31 +0300

    Initial commit

---
+++ src/main/java/ru/joker/spring/PetSpringProject/PetSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.joker.spring.PetSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/joker/spring/PetSpringProject/PetSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.joker.spring.PetSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
