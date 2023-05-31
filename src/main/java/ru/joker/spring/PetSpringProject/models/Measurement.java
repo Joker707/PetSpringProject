f9d961926feabd9fa193f96c16362a816d4658fd
joker <ser.fyodoroff@yandex.ru>
2023-05-31 23:18:59 +0300

    Finish Measurement model class

--- src/main/java/ru/joker/spring/PetSpringProject/models/Measurement.java
+++ src/main/java/ru/joker/spring/PetSpringProject/models/Measurement.java
@@ -5,8 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
@@ -18,19 +23,64 @@ public class Measurement {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
     @Column(name = "value")
-    @NotEmpty(message = "Value shouldn't be empty!")
+    @NotNull(message = "Value shouldn't be empty!")
+    @Min(-100)
+    @Max(100)
     private Double value;
 
     @Column(name = "raining")
-    @NotEmpty(message = "Raining field shouldn't be empty!")
-    private boolean raining;
+    @NotNull(message = "Raining field shouldn't be empty!")
+    private Boolean raining;
 
     @Column(name = "measurement_date_time")
-    @NotEmpty(message = "Date and time shouldn't be empty!")
+    @NotNull(message = "Date and time shouldn't be empty!")
     private LocalDateTime measurementDateTime;
 
+    @NotNull(message = "")
+    @ManyToOne
+    @JoinColumn(name = "sensor", referencedColumnName = "name")
     private Sensor sensor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public LocalDateTime getMeasurementDateTime() {
+        return measurementDateTime;
+    }
+
+    public void setMeasurementDateTime(LocalDateTime measurementDateTime) {
+        this.measurementDateTime = measurementDateTime;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
 }
